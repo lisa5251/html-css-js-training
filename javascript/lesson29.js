144ca040112ec0c293f68f5a702a5b370c8c90bb
lisa5251 <suliqlisa@gmail.com>
2025-01-16 16:15:12 +0100

    lesson 29

---
+++ javascript/lesson29.js
@@ -0,0 +1,11 @@
+var programinglanguages = ['JavaScript', 'Css', 'Html']
+
+console.log(programinglanguages)
+programinglanguages.unshift('Ruby on rails')
+programinglanguages.splice('Ruby')
+
+console.log(math.random*[5]);
+
+var numbers = [1,2,3,4,5,6,7,8]
+console.log(first, second, ...othernumbers)= numbers
+
