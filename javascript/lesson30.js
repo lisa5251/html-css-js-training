4b75b864981055ff528adfccf066776091574eb0
lisa5251 <suliqlisa@gmail.com>
2025-01-23 07:13:26 -0800

    lesson30.1

--- javascript/lesson30.js
+++ javascript/lesson30.js
@@ -1,13 +1,39 @@
-var input = document.getElementById("input");
-var button = document.getElementById("btn");
-var h1 = document.getElementById("h1");
+// var input = document.getElementById("input");
+// var button = document.getElementById("btn");
+// var h1 = document.getElementById("h1");
 
-button.onclick = function(){
-    if (input.value > 10){
-        h1.innerHTML = "Input values is greater than 10.";
-    }else if(input.value < 10){
-        h1.innerHTML = "Input value is less than 10.";
-    }else{
-        h1.innerHTML = "Input value is equal to 10.";
-    }
+// button.onclick = function(){
+//     if (input.value > 10){
+//         h1.innerHTML = "Input values is greater than 10.";
+//     }else if(input.value < 10){
+//         h1.innerHTML = "Input value is less than 10.";
+//     }else{
+//         h1.innerHTML = "Input value is equal to 10.";
+//     }
+// }
+
+function showCurrentTime(){
+    var clock = document.getElementById('clock');
+    var CurrentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes =  currentTime.getMinutes();
+    var seconds =  currentTime.getSeconds();
+
+    var merdian = "AM";
+
+    // if(hours >= noon){
+    //     meridian = "PM";
+    // }
+
+    var clockTime = hours + ":" + minutes + ":" + seconds + " " + meridian;
+    clock.innerText = clockTime;
+
 }
+
+function changeImage(){
+    var time = new Date().getHours();
+    console.log(time);
+}
+
+// var oneSecond = 1000;
+// setInterval(showCurrentTime, oneSecond);
